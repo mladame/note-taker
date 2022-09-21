244d74f3bd5459815f733c8f9f7af71632a33247
Martha A <mladame43@gmail.com>
2022-09-21 00:09:33 -0700

    added get routes, added skeleton for post and delete routes

--- server.js
+++ server.js
@@ -34,10 +34,17 @@ app.use(express.static('public'));
 // app.get("/", function (req,res){})
 // HTML ROUTES
 //TODO GET /notes should return the notes.html file
+app.get("/notes", function (req,res){
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
 //TODO GET * should return the index.html file
+app.get("/", function (req,res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // API ROUTES
 //TODO GET /api/notes should read the db.json file and return all saved notes as JSON
+app.get("/api/notes", (req, res) => res.json(db));
 
 //TODO post routes
 // app.post
@@ -46,12 +53,30 @@ app.use(express.static('public'));
 //TODO      need to find a way to give each note a unique id when it's saved 
 //TODO            (look into npm packages that could do this for you)
 
+app.post("/api/notes", (req, res) => {
+    // get new note to save on body
+
+    // add to db.json
+
+    // return new note
+
+})
+
 // BONUS----------------------------------------------------------------------
 // DELETE /api/notes/:id should receive a query parameter that contains the id 
 //  of a note to delete. To delete a note, you'll need to read all notes from 
 //      the db.json file, remove the note with the given id property, 
 //          and then rewrite the notes to the db.json file
 // ---------------------------------------------------------------------------
+
+app.delete("/api/notes/:id", (req, res) => {
+    // get query param (id) for desired note
+
+    // read all notes from db.json; remove note with matching id
+
+    // rewrite notes to db.json
+    
+})
 
 // app.listen Port
 app.listen(PORT, () =>
